4f117c11d17f4c39d48499274c29e9341d818c0b
pedireddyAharya <aharyapedireddy@gmail.com>
2025-05-27 13:05:51 +0530

    captcha generator.py

--- captcha generator.py
+++ captcha generator.py
@@ -1,10 +1,4 @@
 import random
-
-# Define all possible characters: uppercase, lowercase, digits
 characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-# Generate captcha by picking 5 random characters
 captcha = ''.join(random.choice(characters) for _ in range(5))
-
-# Print the generated captcha
 print("Generated Captcha:", captcha)
